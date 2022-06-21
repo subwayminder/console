2159b5734721776a51d7d66ca5688d703ba590e8
subwayminder <subwayminder@gmail.com>
2022-06-21 14:13:09 +0300

    some fixes

--- src/Input/ArgsInput.php
+++ src/Input/ArgsInput.php
@@ -4,21 +4,6 @@ namespace Subwayminder\Console\Input;
 
 class ArgsInput extends Input
 {
-    private array|null $tokens;
-
-    public function __construct(array $argv = null)
-    {
-        if (null === $argv) {
-            $argv = $_SERVER['argv'];
-        }
-
-        $this->scriptName = array_shift($argv);
-        $this->command = $argv[0];
-        $this->tokens = array_slice($argv, 1);
-        $this->arguments = [];
-        $this->params = [];
-        $this->parse();
-    }
 
     protected function parse(): void
     {
@@ -29,56 +14,5 @@ class ArgsInput extends Input
         }
     }
 
-    private function addArgument(string $token): void
-    {
-        $this->formatString($token, '{}');
-        $argumentsArr = explode(',', $token);
-        foreach ($argumentsArr as $argument) {
-            if ($argument) $this->arguments[] = $argument;
-        }
-    }
 
-    private function addParam(string $token): void
-    {
-        $this->formatString($token, '[]');
-        $paramsArr = explode('=', $token);
-        $param = array_shift($paramsArr);
-        $this->formatString($paramsArr[0], '{}');
-        $valuesArr = explode(',', $paramsArr[0]);
-        $this->addValuesToParam($param, $valuesArr);
-    }
-
-    private function addValuesToParam(string $param, array $values): void
-    {
-        foreach ($values as $value) {
-            $this->params[$param][] = $value;
-        }
-    }
-
-    private function formatString(string &$item, string $pattern): void
-    {
-        $item = trim(trim($item), $pattern);
-    }
-
-    /**
-     * Returns a stringified representation of the args passed to the command.
-     *
-     * @return string
-     */
-//    public function __toString(): string
-//    {
-//        $tokens = array_map(function ($token) {
-//            if (preg_match('{^(-[^=]+=)(.+)}', $token, $match)) {
-//                return $match[1] . $this->escapeToken($match[2]);
-//            }
-//
-//            if ($token && '-' !== $token[0]) {
-//                return $this->escapeToken($token);
-//            }
-//
-//            return $token;
-//        }, $this->tokens);
-//
-//        return implode(' ', $tokens);
-//    }
 }
--- src/Input/Input.php
+++ src/Input/Input.php
@@ -11,6 +11,22 @@ abstract class Input
     protected array $arguments;
     protected string $command;
 
+    protected array|null $tokens;
+
+    public function __construct(array $argv = null)
+    {
+        if (null === $argv) {
+            $argv = $_SERVER['argv'];
+        }
+
+        $this->scriptName = array_shift($argv);
+        $this->command = $argv[0];
+        $this->tokens = array_slice($argv, 1);
+        $this->arguments = [];
+        $this->params = [];
+        $this->parse();
+    }
+
     abstract protected function parse(): void;
 
     public function getCommand(): string
@@ -26,5 +42,36 @@ abstract class Input
     public function getParams(): array
     {
         return $this->params;
+    }
+
+    protected function addArgument(string $token): void
+    {
+        $this->formatString($token, '{}');
+        $argumentsArr = explode(',', $token);
+        foreach ($argumentsArr as $argument) {
+            if ($argument) $this->arguments[] = $argument;
+        }
+    }
+
+    protected function addParam(string $token): void
+    {
+        $this->formatString($token, '[]');
+        $paramsArr = explode('=', $token);
+        $param = array_shift($paramsArr);
+        $this->formatString($paramsArr[0], '{}');
+        $valuesArr = explode(',', trim($paramsArr[0]));
+        $this->addValuesToParam($param, $valuesArr);
+    }
+
+    private function addValuesToParam(string $param, array $values): void
+    {
+        foreach ($values as $value) {
+            $this->params[$param][] = $value;
+        }
+    }
+
+    private function formatString(string &$item, string $pattern): void
+    {
+        $item = trim(trim($item), $pattern);
     }
 }
